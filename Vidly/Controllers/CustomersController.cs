2cbf3b3b3bdb86e4a76b3023d11293740e9d7c47
Matt Wright <matthewbwright@gmail.com>
2019-10-24 10:35:26 -0700

    Sets contraints on Customer Name; adds discount rate to Customer View

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,16 +10,28 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private MyDBContext _context;
+
+        public CustomersController()
+        {
+            _context = new MyDBContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+           _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -26,15 +39,15 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Frodo Baggins" },
-                new Customer { Id = 2, Name = "Samwise Gamegee" },
-                new Customer { Id = 3, Name = "Gandalf the Grey" },
-                new Customer { Id = 4, Name = "Gollum"}
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer { Id = 1, Name = "Frodo Baggins" },
+        //        new Customer { Id = 2, Name = "Samwise Gamegee" },
+        //        new Customer { Id = 3, Name = "Gandalf the Grey" },
+        //        new Customer { Id = 4, Name = "Gollum"}
+        //    };
+        //}
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         // Navigation Type: load an object and its related object together from the database
