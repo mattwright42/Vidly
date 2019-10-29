6183336c02f02833463db7ff2e43b4a251b212e3
Matt Wright <matthewbwright@gmail.com>
2019-10-29 16:21:29 -0700

    Updates the customer view to fetch the data from the server and render it via Datatables

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -27,8 +27,8 @@ namespace Vidly.Controllers
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-            return View(customers);
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View();
         }
 
         public ViewResult New()
