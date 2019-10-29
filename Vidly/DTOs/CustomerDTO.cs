9c6563aef7f4bfa7b0f58080c72d5015af705452
Matt Wright <matthewbwright@gmail.com>
2019-10-29 14:28:17 -0700

    Adds Customer DTO

---
+++ Vidly/DTOs/CustomerDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
+        public DateTime? CustomerBirthdate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        // Navigation Type: load an object and its related object together from the database
+        public MembershipType MembershipType { get; set; }
+
+        // this is treated as a foreign key
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+    }
+}
