e08a9dd24da6986e94a6211dd8766b912bce38b1
Matt Wright <matthewbwright@gmail.com>
2019-10-29 15:49:15 -0700

    Adds delete link; adds jquery for api call to delete

---
+++ Vidly/Controllers/API/MoviesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    public class MoviesController : ApiController
+    {
+        private MyDBContext _context;
+
+        public MoviesController()
+        {
+            _context = new MyDBContext();
+        }
+
+        public IEnumerable<MovieDTO> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDTO>);
+        }
+
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDTO>(movie));
+        }
+        
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDTO movieDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDTO, Movie>(movieDTO);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDTO.Id = movie.Id;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDTO);
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDTO movieDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            Mapper.Map(movieDTO, movieInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
